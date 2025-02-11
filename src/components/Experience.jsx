dbf6723b24c84e7ad8305e58906de1d45dc3ea38
Busroni <busroni10@gmail.com>
2025-02-11 23:13:29 +0700

    add experience components

---
+++ src/components/Experience.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+class Experience extends React.Component{
+
+    render() {
+        return (
+            <div className="Experience-Language text-white flex flex-cols-2 gap-20 -m-15 justify-center">
+                <div className="Experience bg-slate-600 p-15">
+                <h1 className="font-black text-4xl md:text-5xl pb-5 mb-7">Experience</h1>
+                <h1 className="text-2xl font-extrabold"><span className="mr-3">&#9679;</span> 2021-2022</h1>
+                <h2 className="mb-5">Ditalent by Kominfo. IT Suport Google</h2>
+                <h1 className="text-2xl font-extrabold"><span className="mr-3">&#9679;</span> 2021-2022</h1>
+                <h2 className="mb-5">Ditalent by Kominfo. IT Suport Google</h2>
+                <h1 className="text-2xl font-extrabold"><span className="mr-3">&#9679;</span> 2021-2022</h1>
+                <h2 className="mb-5">Ditalent by Kominfo. IT Suport Google</h2>
+                
+
+                </div>
+                <div className="Language bg-slate-500 p-20">
+                <h1 className="font-black text-4xl md:text-5xl pb-5 mb-7">Language</h1>
+                <h2 className="font-semibold text-3xl">Indonesia</h2>
+                <h2 className="font-thin text-xl mb-3">native</h2>
+                <h2 className="font-semibold text-3xl">English</h2>
+                <h2 className="font-thin text-xl mb-3">native</h2>
+                <h2 className="font-semibold text-3xl">Javannese</h2>
+                <h2 className="font-thin text-xl mb-3">native</h2>
+                </div>
+                
+                
+            </div>
+
+        );
+    }
+
+}
+
+export default Experience;
